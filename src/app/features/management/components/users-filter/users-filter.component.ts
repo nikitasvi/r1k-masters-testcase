2587eaeccc0a8789ffd06db8770c94ef120fa5d5
nikita-maltsev <nikitam@segpay.com>
2024-09-27 10:58:12 +0300

    add some validators

--- src/app/features/management/components/users-filter/users-filter.component.ts
+++ src/app/features/management/components/users-filter/users-filter.component.ts
@@ -2,7 +2,6 @@ import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Role } from '../../enums/role.enum';
 import { Status } from '../../enums/status.enum';
-import { UserService } from '../../services/user.service';
 
 @Component({
 	selector: 'app-users-filter',
@@ -10,8 +9,6 @@ import { UserService } from '../../services/user.service';
 	styleUrl: './users-filter.component.scss'
 })
 export class UsersFilterComponent {
-	private userService = inject(UserService);
-
 	@Output() public filterChange = new EventEmitter<any>();
 	@Output() public isFilterOpenedChange = new EventEmitter<boolean>();
 
@@ -22,11 +19,11 @@ export class UsersFilterComponent {
 	public selectedStatus: Status = Status.Active;
 
 	public filterForm = new FormGroup({
-		login: new FormControl(''),
-		phone: new FormControl(''),
+		login: new FormControl('', Validators.pattern('^[a-zA-Z0-9_-]{3,16}$')),
+		phone: new FormControl('', Validators.pattern('^\\+?[0-9]{10,15}$')),
 		dateCreatedAt: new FormControl(''),
 		dateModifiedAt: new FormControl(''),
-		email: new FormControl('', Validators.email),
+		email: new FormControl('', Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')),
 		role: new FormControl(''),
 		status: new FormControl(''),
 	});
